be06043a531020276c46425a4cfc47ff7e6b521d
Yuliya Dubovik <feelgloom@gmail.com>
2025-06-25 15:29:10 +0300

    [feat] chapter 8 - long request simulation, static and dynamic rendeering

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -17,6 +17,7 @@ export async function fetchRevenue() {
     // Don't do this in production :)
 
     console.log('Fetching revenue data...');
+    await new Promise((resolve) => setTimeout(resolve, 3000));
 
     const data = await sql<Revenue[]>`SELECT * FROM revenue`;
 

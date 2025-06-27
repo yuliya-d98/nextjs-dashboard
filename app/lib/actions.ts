75af200eb6938e46e15be071d5d15af4cee6d812
Yuliya Dubovik <feelgloom@gmail.com>
2025-06-28 00:52:57 +0300

    [feat] chapter 13 - error handling, error.tsx anf not-found.tsx

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -26,10 +26,15 @@ export async function createInvoice(formData: FormData) {
   });
   const amountInCents = amount * 100;
   const date = new Date().toISOString().split('T')[0];
-  await sql`
+  try {
+    await sql`
     INSERT INTO invoices (customer_id, amount, status, date)
     VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
   `;
+  } catch(error) {
+    // We'll log the error to the console for now
+    console.error(error);
+  }
   revalidatePath('/dashboard/invoices');
   redirect('/dashboard/invoices');
 }
@@ -45,17 +50,23 @@ export async function updateInvoice(id: string, formData: FormData) {
  
   const amountInCents = amount * 100;
  
-  await sql`
+  try {
+    await sql`
     UPDATE invoices
     SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
     WHERE id = ${id}
   `;
+  } catch(error) {
+    // We'll log the error to the console for now
+    console.error(error);
+  }
  
   revalidatePath('/dashboard/invoices');
   redirect('/dashboard/invoices');
 }
 
 export async function deleteInvoice(id: string) {
+  throw new Error('Failed to Delete Invoice');
   await sql`DELETE FROM invoices WHERE id = ${id}`;
   revalidatePath('/dashboard/invoices');
 }
